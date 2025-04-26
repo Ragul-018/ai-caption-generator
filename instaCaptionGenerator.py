33a7ba7cc9f639921b7f8768446acfb31a5cd097
Ragul-018 <132970906+Ragul-018@users.noreply.github.com>
2025-04-26 21:58:50 +0530

    Update instaCaptionGenerator.py

--- instaCaptionGenerator.py
+++ instaCaptionGenerator.py
@@ -9,7 +9,8 @@ if not api_key:
     api_key = st.text_input("Enter your Gemini API Key: " , type="password")
 
 if api_key:
-    genai.configure(api_key=api_key)
+    # genai.configure(api_key=api_key)
+    genai.configure(api_key=st.secrets["google"]["api_key"])
     reel_description = st.text_area("Describe your reel: ")
     tone = st.selectbox("Choose the type of tone: ",
                         ["Funny", "Trendy", "Inspirational", "Professional"])
